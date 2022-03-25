ca686ff5bdef07f0e384af12155bc5ac5e43ca9b
RCBelmont <dbelmont@foxmail.com>
2022-03-25 17:45:44 +0800

    1.add poll

--- BlenderPlugin/scripts/addons/ABRefresh.py
+++ BlenderPlugin/scripts/addons/ABRefresh.py
@@ -1,20 +1,17 @@
 import bpy
+
 bl_info = {
     "name": "ABTool",
     "description": "ABTool",
-    "author": "RCB",
-    "version": (2, 5, 3),
+    "author": "RCBelmont",
+    "version": (0, 0, 1),
     "blender": (2, 80, 0),
     "location": "",
     "warning": "",
     "doc_url": "",
-    "category": "Mesh",
+    "category": "AssetBrowser",
 }
 
-from bpy.types import Header, Panel, Menu, UIList
-from bpy_extras import (
-    asset_utils,
-)
 
 class Test_Ops(bpy.types.Operator):
     bl_idname = "abtool.tes_ops"
@@ -24,8 +21,8 @@ class Test_Ops(bpy.types.Operator):
     def execute(self, context):
         for file in context.selected_files:
             file.local_id.asset_generate_preview()
-
         return {'FINISHED'}
+
 
 class ASSETBROWSER_PT_tool(bpy.types.Panel):
     bl_space_type = 'FILE_BROWSER'
@@ -33,15 +30,20 @@ class ASSETBROWSER_PT_tool(bpy.types.Panel):
     bl_label = "Tool"
     bl_category = 'Tool'
 
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.browse_mode == 'ASSETS'
+
     def draw(self, context):
         layout = self.layout
-        layout.operator("ed.lib_id_generate_preview", icon='FILE_REFRESH', text="")
-        layout.operator("abtool.tes_ops", text="RR")
+        layout.operator("abtool.tes_ops", text="RefreshSelect")
+
 
 class_to_register = (
     Test_Ops,
     ASSETBROWSER_PT_tool,
 )
+
 
 def register():
     for c in class_to_register:
