a97317490cf77a09fbce889eaae61c6b711f5ff8
RCBelmont <dbelmont@foxmail.com>
2022-07-27 16:26:56 +0800

    Update Node Config
    Update Node Config Script For 3.2.x

--- SourceCodeTool/ConfigNode.py
+++ SourceCodeTool/ConfigNode.py
@@ -1,15 +1,20 @@
 import os.path
+from datetime import date
 from enum import Enum
 
+###
+project_path = "F:\\DevSSD\\Blender\\BlenderDev\\build_windows_Full_x64_vc16_Release\\"
 node_c_path = r"source\blender\nodes\shader\nodes"
 node_glsl_path = r"source\blender\gpu\shaders\material"
 bke_node_path = r"source\blender\blenkernel\BKE_node.h"
 nod_static_types_path = r"source\blender\nodes\NOD_static_types.h"
 nod_shader_path = r"source\blender\nodes\NOD_shader.h"
 node_cc_path = r"source\blender\blenkernel\intern\node.cc"
-gpu_material_lib_path = r"source\blender\gpu\intern\gpu_material_library.c"
+# deprecated
+# gpu_material_lib_path = r"source\blender\gpu\intern\gpu_material_library.c"
+gpu_source_list = project_path + r"source\blender\gpu\glsl_gpu_source_list.h"
 gpu_cmake_path = r"source\blender\gpu\CMakeLists.txt"
-node_cmake_path = r"source\blender\nodes\CMakeLists.txt"
+node_cmake_path = r"source\blender\nodes\shader\CMakeLists.txt"
 startup_py_path = r"release\scripts\startup\nodeitems_builtins.py"
 
 
@@ -25,6 +30,7 @@ class NodeType(Enum):
     Group = "Group"
     Layout = "Layout"
 
+
 class PollType(Enum):
     none = ""
     line_style_shader_nodes_poll = "line_style_shader_nodes_poll"
@@ -36,7 +42,6 @@ class PollType(Enum):
     object_cycles_shader_nodes_poll = "object_cycles_shader_nodes_poll"
     object_eevee_shader_nodes_poll = "object_eevee_shader_nodes_poll"
     object_eevee_cycles_shader_nodes_poll = "object_eevee_cycles_shader_nodes_poll"
-
 
 
 def contain_find(target_list, find_content):
@@ -70,6 +75,7 @@ def find_func_end(target_list, func_name):
 
 node_type = NodeType.Shader
 poll_type = PollType.eevee_shader_nodes_poll
+author_name = 'RCB'
 # bsdf_two_side
 node_kw = "bsdf_new_shading"
 # bsdf two side
@@ -80,6 +86,14 @@ node_kw2 = node_kw1.title()
 node_kw3 = node_kw.upper()
 # BsdfTwoSide
 node_kw4 = node_kw2.replace(" ", "")
+# node_bsdf_two_side
+shader_func_name = 'node_' + node_kw
+# register_node_type_sh_bsdf_two_side
+node_register_func_name = 'register_node_type_sh_' + node_kw
+# node_shader_bsdf_two_side_cc
+name_space = "node_shader_" + node_kw + "_cc"
+# gpu_shader_material__bsdf_two_side
+shader_file_name = 'gpu_shader_material_' + node_kw
 
 
 def add_node_macro():
@@ -98,55 +112,71 @@ def add_node_macro():
     last_define = contents[end_line - 1]
     num = last_define.split(' ')[2]
     now_num = int(num) + 1
-    contents.insert(end_line, "#define SH_NODE_" + node_kw.upper() + " " + str(now_num) + "\n")
+    contents.insert(end_line, "#define SH_NODE_" + node_kw3 + " " + str(now_num) + "\n")
     f = open(bke_node_path, 'w')
     f.writelines(contents)
     f.close()
 
 
 def add_node_c():
-    f = open(node_c_path + "/node_shader_" + node_kw + ".c", 'a')
-    in_func = "sh_node_" + node_kw + "_in"
-    out_func = "sh_node_" + node_kw + "_out"
-    gpu_func = "node_shader_gpu_" + node_kw
-    f.write("#include \"../node_shader_util.h\"\n")
-    f.write("static bNodeSocketTemplate " + in_func + "[] = {\n")
-    f.write("\t{SOCK_RGBA, N_(\"TestColor\"), 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, PROP_FACTOR},\n")
-    f.write("\t{-1, \"\"},\n")
-    f.write("};\n")
-    f.write("static bNodeSocketTemplate " + out_func + "[] = {\n")
-    f.write("\t{SOCK_SHADER, N_(\"BSDF\")},\n")
-    f.write("\t{-1, \"\"},\n")
-    f.write("};\n")
+    today = date.today()
+    f = open(node_c_path + "/node_shader_" + node_kw + ".cc", 'a')
+    # Write Comment
+    f.write('//=====================================\n')
+    f.write('//' + node_kw2 + '\n')
+    f.write('//Created By ' + author_name + '\n')
+    f.write('//' + " " + str(today) + '\n')
+    f.write('//=====================================\n')
+    # Add 'Include'
+    f.write("#include \"node_shader_util.hh\"\n")
+    # Begin NameSpace
+    f.write('namespace blender::nodes::' + name_space + '\n')
+    f.write('{\n')
+    # Begin Declare Function
+    f.write('\tstatic void node_declare(NodeDeclarationBuilder& b)\n')
+    f.write('\t{\n')
+    f.write('\t\tb.add_input<decl::Color>(N_(\"Color\"))\n')
+    f.write('\t\t .default_value({0.8f, 0.8f, 0.8f, 1.0f});\n')
+    f.write('\t\tb.add_input<decl::Float>(N_("Weight")).unavailable();\n')
+    f.write('\t\t b.add_output<decl::Shader>(N_("BSDF"));\n')
+    f.write('\t}\n')
+    # End Declare Function
+    # Begin Gpu Function
     f.write(
-        "static int " + gpu_func + "(GPUMaterial* mat,  bNode* node,  bNodeExecData*UNUSED(execdata),  GPUNodeStack* in, GPUNodeStack* out)\n")
-    f.write("{\n")
-    f.write("\treturn GPU_stack_link(mat,node, \"shader_func_" + node_kw + "\", in, out);\n")
-    f.write("};\n")
-    ##
-    f.write("void register_node_type_sh_" + node_kw + "(void)\n")
-    f.write("{\n")
-    f.write("\tstatic bNodeType ntype;\n")
-    f.write("\tsh_node_type_base(&ntype, SH_NODE_" + node_kw3 + ", \"" + node_kw2 + "\", NODE_CLASS_SHADER, 0);" + "\n")
+        '\tstatic int node_shader_' + node_kw + '(GPUMaterial* mat, bNode* node, bNodeExecData*UNUSED(execdata), GPUNodeStack* in, GPUNodeStack* out)\n')
+    f.write('\t{\n')
+    f.write('\t\tGPU_material_flag_set(mat, GPU_MATFLAG_DIFFUSE|GPU_MATFLAG_GLOSSY);\n')
+    f.write('\t\treturn GPU_stack_link(mat, node, \"' + shader_func_name + '\", in, out);\n')
+    f.write('\t}\n')
+    # End Gpu Function
+    f.write('}\n')
+    # End NameSpace
+    f.write('/* node type definition */\n')
+    f.write('void ' + node_register_func_name + '()\n')
+    f.write('{\n')
+    f.write('\tnamespace file_ns = blender::nodes::' + name_space + ';\n')
+    f.write('\tstatic bNodeType ntype;\n')
     f.write(
-        "\tnode_type_socket_templates(&ntype, " + in_func + ", " + out_func + ");\n")
-    f.write("\tnode_type_size_preset(&ntype, NODE_SIZE_LARGE);\n")
-    f.write("\tnode_type_init(&ntype, NULL);\n")
-    f.write("\tnode_type_storage(&ntype, \"\", NULL, NULL);\n")
-    f.write("\tnode_type_gpu(&ntype, " + gpu_func + ");\n")
-    f.write("\tnodeRegisterType(&ntype);\n")
-    f.write("}\n")
+        '\tsh_node_type_base(&ntype, SH_NODE_' + node_kw3 + ', ' + '\"' + node_kw2 + '\"' + ', ' + 'NODE_CLASS_SHADER);\n')
+    f.write('\tntype.declare = file_ns::node_declare;\n')
+    f.write('\tnode_type_size_preset(&ntype, NODE_SIZE_LARGE);\n')
+    f.write('\tnode_type_init(&ntype, NULL);\n')
+    f.write('\tnode_type_storage(&ntype, "", NULL, NULL);\n')
+    f.write('\tnode_type_gpu(&ntype, file_ns::node_shader_' + node_kw + ');\n')
+    f.write('\tnodeRegisterType(&ntype);\n')
+    f.write('}\n')
     f.close()
 
 
 def shader_file():
-    f = open(node_glsl_path + "/gpu_shader_material_" + node_kw + ".glsl", 'a')
-    f.write("#ifndef VOLUMETRICS\n")
-    f.write("void shader_func_" + node_kw + "(vec4 TestColor, out Closure result)\n")
+    f = open(node_glsl_path + '/' + shader_file_name + ".glsl", 'a')
+    f.write("\n")
+    f.write('void ' + shader_func_name + "(vec4 TestColor, float weight, out Closure result)\n")
     f.write("{\n")
     f.write("\tresult.radiance = vec3(TestColor.r, TestColor.g, TestColor.b);\n")
+    f.write("\tresult.radiance *= weight;\n")
     f.write("}\n")
-    f.write("#endif\n")
+    f.write("\n")
     f.close()
 
 
@@ -154,7 +184,7 @@ def add_node_def():
     f = open(nod_static_types_path, 'r')
     contents = f.readlines()
     f.close()
-    begin_line = contain_find(contents, "SH_NODE_OUTPUT_AOV")
+    begin_line = contain_find(contents, "SH_NODE_CURVE_FLOAT")
     if begin_line == -1:
         raise RuntimeError("add_node_def error")
     begin_line += 1
@@ -173,7 +203,7 @@ def add_register_declear():
     if begin_line == -1:
         raise RuntimeError("add_register_declear error")
     begin_line += 1
-    insert_content = "void register_node_type_sh_" + node_kw + "(void);\n"
+    insert_content = 'void ' + node_register_func_name + '(void);\n'
     contents.insert(begin_line, insert_content)
     f = open(nod_shader_path, 'w')
     f.writelines(contents)
@@ -190,37 +220,50 @@ def add_register_call():
     target_line = find_func_end(contents, "registerShaderNodes")
     if target_line == -1:
         raise RuntimeError("add_register_call error")
-    insert_content = "\tregister_node_type_sh_" + node_kw + "();\n"
+    insert_content = '\t' + node_register_func_name + '();\n'
     contents.insert(target_line, insert_content)
     f = open(node_cc_path, 'w')
     f.writelines(contents)
     f.close()
 
 
-def config_mat_lib():
-    f = open(gpu_material_lib_path, 'r')
+# deprecated
+# def config_mat_lib():
+#     f = open(gpu_material_lib_path, 'r')
+#     contents = f.readlines()
+#     f.close()
+#     line1 = contain_find(contents, "datatoc_gpu_shader_material_world_normals_glsl")
+#
+#     if line1 == -1:
+#         raise RuntimeError("config_mat_lib error")
+#     insert_str1 = "extern char datatoc_gpu_shader_material_" + node_kw + "_glsl[];\n"
+#     contents.insert(line1 + 1, insert_str1)
+#
+#     line2 = find_func_end(contents, "gpu_material_libraries[]")
+#     if line2 == -1:
+#         raise RuntimeError("config_mat_lib error")
+#     insert_str2 = "\t\t&gpu_shader_material_" + node_kw + "_library,\n"
+#     contents.insert(line2, insert_str2)
+#
+#     line3 = contain_find(contents, "GPUMaterialLibrary *gpu_material_libraries[]")
+#     if line3 == -1:
+#         raise RuntimeError("config_mat_lib error")
+#     insert_str3 = "static GPUMaterialLibrary gpu_shader_material_" + node_kw + "_library = {\n" + \
+#                   "\t.code = datatoc_gpu_shader_material_" + node_kw + "_glsl,\n" + "\t.dependencies = {NULL},\n" + "};\n"
+#     contents.insert(line3 - 1, insert_str3)
+#     f = open(gpu_material_lib_path, 'w')
+#     f.writelines(contents)
+#     f.close()
+#
+
+
+def add_gpu_source_list():
+    f = open(gpu_source_list, 'r')
     contents = f.readlines()
     f.close()
-    line1 = contain_find(contents, "datatoc_gpu_shader_material_world_normals_glsl")
-
-    if line1 == -1:
-        raise RuntimeError("config_mat_lib error")
-    insert_str1 = "extern char datatoc_gpu_shader_material_" + node_kw + "_glsl[];\n"
-    contents.insert(line1 + 1, insert_str1)
-
-    line2 = find_func_end(contents, "gpu_material_libraries[]")
-    if line2 == -1:
-        raise RuntimeError("config_mat_lib error")
-    insert_str2 = "\t\t&gpu_shader_material_" + node_kw + "_library,\n"
-    contents.insert(line2, insert_str2)
-
-    line3 = contain_find(contents, "GPUMaterialLibrary *gpu_material_libraries[]")
-    if line3 == -1:
-        raise RuntimeError("config_mat_lib error")
-    insert_str3 = "static GPUMaterialLibrary gpu_shader_material_" + node_kw + "_library = {\n" + \
-                  "\t.code = datatoc_gpu_shader_material_" + node_kw + "_glsl,\n" + "\t.dependencies = {NULL},\n" + "};\n"
-    contents.insert(line3 - 1, insert_str3)
-    f = open(gpu_material_lib_path, 'w')
+    insert_str = '\nSHADER_SOURCE(datatoc_' + shader_file_name + '_glsl, ' + '\"' + shader_file_name + '.glsl\", ' + '\"shaders/material/' + shader_func_name + '.glsl\")'
+    f = open(gpu_source_list, 'w')
+    contents.append(insert_str)
     f.writelines(contents)
     f.close()
 
@@ -229,10 +272,10 @@ def config_gpu_cmake_file():
     f = open(gpu_cmake_path, 'r')
     contents = f.readlines()
     f.close()
-    line = contain_find(contents, "data_to_c_simple(shaders/material/gpu_shader_material_world_normals.glsl SRC)")
+    line = contain_find(contents, "shaders/material/gpu_shader_material_world_normals.glsl")
     if line == -1:
         raise RuntimeError("config_cmake_file error")
-    insert_str = "\ndata_to_c_simple(shaders/material/" + "gpu_shader_material_" + node_kw + ".glsl SRC)\n"
+    insert_str = "  shaders/material/" + shader_file_name + ".glsl\n"
     contents.insert(line + 1, insert_str)
     f = open(gpu_cmake_path, 'w')
     f.writelines(contents)
@@ -243,10 +286,10 @@ def config_node_cmake_file():
     f = open(node_cmake_path, 'r')
     contents = f.readlines()
     f.close()
-    line = contain_find(contents, "shader/node_shader_util.c")
+    line = contain_find(contents, "nodes/node_shader_wireframe.cc")
     if line == -1:
         raise RuntimeError("config_node_cmake_file error")
-    insert_str = "\n\tshader/nodes/" + "node_shader_" + node_kw + ".c\n"
+    insert_str = "  nodes/" + "node_shader_" + node_kw + ".cc\n"
     contents.insert(line + 1, insert_str)
     f = open(node_cmake_path, 'w')
     f.writelines(contents)
@@ -263,11 +306,12 @@ def config_startup_script():
     if poll_type == PollType.none:
         insert_str = "\t\tNodeItem(\"ShaderNode" + node_kw4 + "\"),\n"
     else:
-        insert_str = "\t\tNodeItem(\"ShaderNode" + node_kw4 + "\", poll=" + str(poll_type.value) +"),\n"
+        insert_str = "\t\tNodeItem(\"ShaderNode" + node_kw4 + "\", poll=" + str(poll_type.value) + "),\n"
     contents.insert(line + 1, insert_str)
     f = open(startup_py_path, 'w')
     f.writelines(contents)
     f.close()
+
 
 if __name__ == '__main__':
     # print(node_kw)
@@ -275,27 +319,14 @@ if __name__ == '__main__':
     # print(node_kw2)
     # print(node_kw3)
     # print(node_kw4)
-
     add_node_macro()
     add_node_c()
     shader_file()
     add_node_def()
     add_register_declear()
     add_register_call()
-    config_mat_lib()
     config_gpu_cmake_file()
     config_node_cmake_file()
+    add_gpu_source_list()
     config_startup_script()
-
-##print(os.path.abspath(node_c_path))
-##add_node_macro()
-# f = open(node_cc_path, "r")
-# contents = f.readlines()
-# f.close()
-#
-# # TODO:DELETE PRINT
-# contents.insert(end_idx, "\ttest_func();\n")
-# print(end_idx)
-# f = open(node_cc_path, "w")
-# f.write("".join(contents))
-# f.close()
+    print("Shader: " + node_kw + " Add Done!!")
